1a3072a3916e875c19126a80063cbb2ffcdd17eb
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2019-11-13 22:11:34 +0600

    Initial commit

---
+++ src/main/java/ru/otus/springquartzexample/SpringQuartzExampleApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.springquartzexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuartzExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringQuartzExampleApplication.class, args);
+    }
+
+}
